ee7bb0b80ac7726b604c338cc706c336e88e41d9
Thanawut Naetisoonthon <tour@ziios.com>
2016-08-03 18:31:31 +0700

    Application Exit event handle

---
+++ WebApplication1/scripts/index.js
@@ -0,0 +1,22 @@
+﻿function btnResetStatusClick() {
+    Windows.Storage.ApplicationData.current.localSettings.values["data"] = "reset";
+    document.getElementById("lblStatus").innerText = Windows.Storage.ApplicationData.current.localSettings.values["data"];
+}
+function btnRegetStatusClick() {
+    document.getElementById("lblStatus").innerText = Windows.Storage.ApplicationData.current.localSettings.values["data"];
+}
+function btnOpenWindowClick() {
+    window.open("index.html");
+}
+
+function start() {
+    document.getElementById("lblStatus").innerText = Windows.Storage.ApplicationData.current.localSettings.values["data"];
+    document.getElementById("btnResetStatus").addEventListener("click", btnResetStatusClick);
+    document.getElementById("btnRegetStatus").addEventListener("click", btnRegetStatusClick);
+    document.getElementById("btnOpenWindow").addEventListener("click", btnOpenWindowClick);
+}
+
+start();
+window.onbeforeunload = function onbeforeunloadEvent() {
+    Windows.Storage.ApplicationData.current.localSettings.values["data"] = "set data from onbeforeunload event";
+}
